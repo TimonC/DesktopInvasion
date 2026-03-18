5837166a46fa5fb2a920a8aae0dd8ec7ff636990
desktopinvasion <desktopinvasion@gmail.com>
2026-03-18 16:17:46 +0100

    Reset wild pokemon upon defeat

--- src/Game.cpp
+++ src/Game.cpp
@@ -271,21 +271,6 @@ void Game::handleBattleEnd(const char* endState) {
     bool removeWild = playerWon || opponentCaught || opponentWon;
 
     if (removeWild) {
-        // Player won the battle - add XP
-        if (playerWon) {
-            if (m_db.addPokemonXp(m_partyIds[0], 100)) {
-                int newXp = m_db.getPokemonXp(m_partyIds[0]);
-                qDebug() << "Added 100 XP to fighting Pokemon. Total XP:" << newXp;
-            } else {
-                qWarning() << "Failed to add XP to Pokemon ID:" << m_partyIds[0];
-            }
-            if( m_db.clearWild()){
-                qDebug() << "Cleared wild pokemon instance";
-            }else{
-                qWarning() << "Failed to clear wild pokemon";
-            }
-        }
-
         // Handle catching
         if (opponentCaught) {
             int caughtId = m_db.catchWildPokemon();
@@ -307,16 +292,31 @@ void Game::handleBattleEnd(const char* endState) {
                     }
                 }
             }
-        }
+        }else{
+            // Player won the battle - add XP
+            if (playerWon) {
+                if (m_db.addPokemonXp(m_partyIds[0], 100)) {
+                    int newXp = m_db.getPokemonXp(m_partyIds[0]);
+                    qDebug() << "Added 100 XP to fighting Pokemon. Total XP:" << newXp;
+                } else {
+                    qWarning() << "Failed to add XP to Pokemon ID:" << m_partyIds[0];
+                }
+            }
 
-        // Cleanup
-        if (m_activeBattle)  safelyRemoveBattleScene();
-        if (m_wildPokemon) {
-            m_wildPokemon->deleteLater();
-            m_wildPokemon = nullptr;
+            if( m_db.clearWild()){
+                qDebug() << "Cleared wild pokemon instance";
+            }else{
+                qWarning() << "Failed to clear wild pokemon";
+            }
+            // Cleanup
+            if (m_activeBattle)  safelyRemoveBattleScene();
+            if (m_wildPokemon) {
+                m_wildPokemon->deleteLater();
+                m_wildPokemon = nullptr;
+            }
+            m_spawnPoint = QPoint(-1,-1);
+            m_spawnTimer->start();
         }
-        m_spawnPoint = QPoint(-1,-1);
-        m_spawnTimer->start();
 
     } else {
         // Battle ended without removing wild Pokemon
