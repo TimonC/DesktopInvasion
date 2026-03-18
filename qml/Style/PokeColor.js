92d1d4abb456d915108184a3db1e08e53979f17f
desktopinvasion <desktopinvasion@gmail.com>
2026-03-18 16:18:25 +0100

    Slightly darker lightColor

--- qml/Style/PokeColor.js
+++ qml/Style/PokeColor.js
@@ -24,7 +24,7 @@ function typeColor(typeName) {
 }
 
     function lighter(baseColor) {
-        var colorRatio = 1.1
+        var colorRatio = 1.05
         var c = Qt.color(baseColor)
         return Qt.rgba(
             Math.min(1, c.r * colorRatio),
