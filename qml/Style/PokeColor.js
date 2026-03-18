d4cbb1649ae6676b8f964d8965c3c2d58eae5590
desktopinvasion <desktopinvasion@gmail.com>
2026-03-18 16:18:26 +0100

    Source and sort the pokemon color

--- qml/Style/PokeColor.js
+++ qml/Style/PokeColor.js
@@ -1,24 +1,25 @@
 .pragma library
 
+// https://bulbapedia.bulbagarden.net/wiki/Help:Color_templates
 function typeColor(typeName) {
     switch(typeName) {
-        case "Normal":   return "#9FA19F"
-        case "Fighting": return "#FF8000"
-        case "Flying":   return "#81B9EF"
-        case "Poison":   return "#9141CB"
-        case "Ground":   return "#E0C068";
-        case "Rock":     return "#AFA981"
         case "Bug":      return "#91A119"
-        case "Ghost":    return "#704170"
-        case "Steel":    return "#60A1B8"
-        case "Fire":     return "#E62829"
-        case "Water":    return "#2980EF"
-        case "Grass":    return "#3FA129"
-        case "Electric": return "#FAC000"
-        case "Psychic":  return "#EF4179"
-        case "Ice":      return "#3DCEF3"
-        case "Dragon":   return "#5060E1"
         case "Dark":     return "#624D4E"
+        case "Dragon":   return "#5060E1"
+        case "Electric": return "#FAC000"
+        case "Fighting": return "#FF8000"
+        case "Fire":     return "#E62829"
+        case "Flying":   return "#81B9EF"
+        case "Ghost":    return "#704170"
+        case "Grass":    return "#3FA129"
+        case "Ground":   return "#915121";
+        case "Ice":      return "#3DCEF3"
+        case "Normal":   return "#9FA19F"
+        case "Poison":   return "#9141CB"
+        case "Psychic":  return "#EF4179"
+        case "Rock":     return "#AFA981"
+        case "Steel":    return "#60A1B8"
+        case "Water":    return "#2980EF"
         default:         return "transparent"
     }
 }
