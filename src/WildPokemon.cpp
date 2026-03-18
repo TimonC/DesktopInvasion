b177dbaade51007786a1b6f35fccdf2bb4e5804e
desktopinvasion <desktopinvasion@gmail.com>
2026-03-18 16:16:21 +0100

    Remove battlescene loader from wildpokemon

--- src/WildPokemon.cpp
+++ src/WildPokemon.cpp
@@ -22,7 +22,7 @@ WildPokemon::WildPokemon(const PokemonInfo* info, QWindow *parent)
     const QRect& screen = Globals::screenGeometry();
     setPosition(QPoint(screen.width()/2, screen.height()/2));
 
-    setSource(QUrl("qrc:/sprites/PokemonSprite.qml"));
+    setSource(QUrl("qrc:/sprites/WildPokemon.qml"));
     m_sprite = rootObject();
     m_sprite->setProperty("spriteSheet", QString("qrc:/assets/HGSS/PokGen%1_transparent_reordered.png").arg(info->generation));
     m_sprite->setProperty("scaleFactor", Globals::SCALE);
