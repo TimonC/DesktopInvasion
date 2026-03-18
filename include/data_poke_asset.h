ae890e5cdb597a8cce0e1e35c0be30e63daf1d8b
desktopinvasion <desktopinvasion@gmail.com>
2026-03-18 16:19:04 +0100

    Beautiful .src generation

--- include/data_poke_asset.h
+++ include/data_poke_asset.h
@@ -14,24 +14,5 @@ struct asset_info{
 
 extern const asset_info* const kAssetInfo[493];
 
-
-// Large Pokemon IDs
-int BIG_IDS[] = {
-    208,  // Steelix
-    249,  // Lugia
-    250,  // Ho-Oh
-    483,  // Dialga
-    484,  // Palkia
-    486,  // Regigigas
-    487,  // Giratina
-    493,  // Arceus
-    321,  // Wailord
-    382,  // Kyogre
-    383,  // Groudon
-    384,  // Rayquaza
-};
-
-// Generation limits (number of unique Pokemon per generation)
-int GEN_LIMS[] = {151, 100, 135, 107};
 #endif
 
