0737bda8bb598a15e178a01fa00267ea6db8046b
desktopinvasion <desktopinvasion@gmail.com>
2026-03-18 16:16:23 +0100

    Store id for online database entity connection

--- include/GameState.h
+++ include/GameState.h
@@ -1,18 +1,48 @@
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
-#include <QPoint>
-#include <array>
 
-struct GameState{
-    int gameId;
-    std::array<int, 6> party;
-    struct WildPokemonState;
+class QPoint;
+enum class StatType {
+    HP,
+    Attack,
+    Defense,
+    SpecialAttack,
+    SpecialDefense,
+    Speed,
+    COUNT
 };
 
-struct WildPokemonState{
-    int direction;
-    QPoint* position;
-    int pokedexId;
+enum class Nature {
+    Hardy, Docile, Serious, Bashful, Quirky,
+    Lonely, Adamant, Naughty, Brave,
+    Bold, Impish, Lax, Relaxed,
+    Modest, Mild, Rash, Quiet,
+    Calm, Gentle, Careful, Sassy,
+    Timid, Hasty, Jolly, Naive
+};
+
+struct PokemonState{
+    int _id;
+    int pokedex_id;
+    int variant_id = 0;
+    const char* name;
+
+    StatType ivs[6];
+    StatType evs[6];
+    Nature nature;
+
+    int moves[4];
+    int total_xp;
+};
+
+
+struct GameState{
+    const int _id;
+    const int wild_pokemon_id = 0; //There is only every one wild pokemon, queryable by '0'
+
+    int player_sprite_id;
+    const char* name;
+    int party_id[6];
 };
 
 #endif
--- src/GameState.cpp
+++ src/GameState.cpp
@@ -1,2 +1,3 @@
 #include <GameState.h>
 #include <QPoint>
+
