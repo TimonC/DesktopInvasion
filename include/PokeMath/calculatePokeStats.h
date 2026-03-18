643604bbe077f7e43ab75af52c0d63dab40ecc61
desktopinvasion <desktopinvasion@gmail.com>
2026-03-18 16:18:25 +0100

    Fix index errors in stat calc

--- include/PokeMath/calculatePokeStats.h
+++ include/PokeMath/calculatePokeStats.h
@@ -4,11 +4,11 @@
 #include <array>
 
 /* https://bulbapedia.bulbagarden.net/wiki/Stat#Determination_of_stats
-    For gen 4  (i.e. gen 3 upwards)
+    For gen 3  (i.e. gen 3 upwards)
 */
 
 inline constexpr int calculateStat(int lvl, int base, int iv, int ev) noexcept {
-    return (lvl * (2 * base + iv + ev / 4)) / 100;
+    return (lvl * (1 * base + iv + ev / 4)) / 100;
 }
 
 inline std::array<int, 6> calculatePokeStats(
@@ -24,11 +24,12 @@ inline std::array<int, 6> calculatePokeStats(
     result[0] = 10 + lvl + calculateStat(lvl, baseStats[0], ivs[0], evs[0]);
 
     // Unrolled loop (static cast to int floors)
-    result[1] = static_cast<int>(calculateStat(lvl, baseStats[1], ivs[1], evs[1]) * nature[0]);
-    result[2] = static_cast<int>(calculateStat(lvl, baseStats[2], ivs[2], evs[2]) * nature[1]);
-    result[3] = static_cast<int>(calculateStat(lvl, baseStats[3], ivs[3], evs[3]) * nature[2]);
-    result[4] = static_cast<int>(calculateStat(lvl, baseStats[4], ivs[4], evs[4]) * nature[3]);
-    result[5] = static_cast<int>(calculateStat(lvl, baseStats[5], ivs[5], evs[5]) * nature[4]);
+    result[1] = static_cast<int>(nature[0] * (5 + calculateStat(lvl, baseStats[1], ivs[1], evs[1])));
+    result[2] = static_cast<int>(nature[1] * (5 + calculateStat(lvl, baseStats[2], ivs[2], evs[2])));
+    result[3] = static_cast<int>(nature[2] * (5 + calculateStat(lvl, baseStats[3], ivs[3], evs[3])));
+    result[4] = static_cast<int>(nature[3] * (5 + calculateStat(lvl, baseStats[4], ivs[4], evs[4])));
+    result[5] = static_cast<int>(nature[4] * (5 + calculateStat(lvl, baseStats[5], ivs[5], evs[5])));
+
     return result;
 }
 
