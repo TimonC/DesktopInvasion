7944504a49c0ae24d123c9178b9f0fbf0c8caeb5
desktopinvasion <desktopinvasion@gmail.com>
2026-03-18 16:19:49 +0100

    Slightly longer ball open delay

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     /* Globals::debug(true); */
-    float scale = 4;
+    float scale = 2;
     float speed = 1;
     Globals::scale(scale);
     Globals::animationSpeed(speed);
