350c3efb2d0252c1d50eec697e98a39b8f1e5ea7
desktopinvasion <desktopinvasion@gmail.com>
2026-03-19 16:41:58 +0100

    Tidied up main file

--- src/main.cpp
+++ src/main.cpp
@@ -16,37 +16,30 @@
 
 
 int main(int argc, char *argv[]) {
-
-    const char* valgrindMode = std::getenv("VALGRIND_MODE");
-    bool isValgrindMode = (valgrindMode && strcmp(valgrindMode, "1") == 0);
-
-    QString dockerEnv = qEnvironmentVariable("DOCKER_ENV");
-    bool isDev = (dockerEnv == "dev");
-
-    const bool DOOM_TIMER = isDev && isValgrindMode;
-    const int DOOM_S = 60;
-
-    if (isDev) {
-        qDebug() << "=== RUNNING IN DEV MODE ===";
-        if (isValgrindMode) {
-            qDebug() << "=== VALGRIND DEBUG MODE ENABLED ===";
-            qDebug() << "Will auto-exit after" << DOOM_S << "seconds";
-        }
-    }
-
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName("DesktopInvasion");
     QCoreApplication::setApplicationName("DesktopInvasion");
+
     QIcon icon;
     icon = QIcon(":/assets/icon/icon.png");
     app.setWindowIcon(icon);
+
+
+    QOpenGLContext context;
+    if (context.create()) {
+        qDebug() << "OpenGL IS WORKING! Version:"
+                 << context.format().majorVersion() << "." << context.format().minorVersion();
+        qDebug() << "OpenGL profile:" << context.format().profile();
+        qDebug() << "OpenGL renderable:" << context.format().renderableType();
+    } else {
+        qDebug() << "OpenGL FAILED - using software rendering";
+    }
 
     int pixelFontId = QFontDatabase::addApplicationFont(":/assets/fonts/PressStart2P-Regular.ttf");
     int dotGothicId = QFontDatabase::addApplicationFont(":/assets/fonts/DotGothic16-Regular.ttf");
 
     QString pixelFontFamily;
     QString dotGothicFamily;
-
     if (pixelFontId != -1) {
         QStringList families = QFontDatabase::applicationFontFamilies(pixelFontId);
         if (!families.isEmpty()) {
@@ -54,7 +47,6 @@ int main(int argc, char *argv[]) {
             qDebug() << "Loaded PressStart2P font:" << pixelFontFamily;
         }
     }
-
     if (dotGothicId != -1) {
         QStringList families = QFontDatabase::applicationFontFamilies(dotGothicId);
         if (!families.isEmpty()) {
@@ -67,27 +59,7 @@ int main(int argc, char *argv[]) {
     engine.rootContext()->setContextProperty("pixelFontFamily", pixelFontFamily);
     engine.rootContext()->setContextProperty("dotGothicFontFamily", dotGothicFamily);
 
-    QOpenGLContext context;
-    if (context.create()) {
-        qDebug() << "OpenGL IS WORKING! Version:"
-                 << context.format().majorVersion() << "." << context.format().minorVersion();
-        qDebug() << "OpenGL profile:" << context.format().profile();
-        qDebug() << "OpenGL renderable:" << context.format().renderableType();
-    } else {
-        qDebug() << "OpenGL FAILED - using software rendering";
-    }
-
     QString dbDir;
-
-#ifdef Q_OS_WIN
-    dbDir = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
-#else
-    QString appDirPath = qEnvironmentVariable("APPDIR");
-    dbDir = appDirPath.isEmpty()
-        ? QCoreApplication::applicationDirPath()
-        : QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
-#endif
-
     QDir dir(dbDir);
     if (!dir.exists()) {
         if (!dir.mkpath(".")) {
@@ -97,18 +69,21 @@ int main(int argc, char *argv[]) {
         qDebug() << "Created database directory:" << dbDir;
     }
 
+    QObject::connect(&app, &QApplication::aboutToQuit, []() {
+        PokemonDatabase::instance().shutdown();
+    });
     QString dbPath = QDir(dbDir).filePath("pokemon.db");
-    qDebug() << "Initializing database at:" << dbPath;
-
-        // In main.cpp:
     if (!PokemonDatabase::instance().initialize(dbPath)) {
         qCritical() << "Failed to initialize database at:" << dbPath;
         return 1;
     }
     qDebug() << "Database initialized successfully";
 
-    std::unique_ptr<Game> game = std::make_unique<Game>(&engine, nullptr);
 
+    bool isDev = (qEnvironmentVariable("DOCKER_ENV") == "dev");
+    const bool DOOM_TIMER = isDev && (strcmp(std::getenv("VALGRIND_MODE"), "1") == 0);
+    const int DOOM_S = 60;
+    if (isDev) qDebug() << "=== RUNNING IN DEV MODE ===";
     if (DOOM_TIMER) {
         qDebug() << "=== VALGRIND DEBUG MODE ===";
         qDebug() << "Game will auto-exit in" << DOOM_S << "seconds";
@@ -120,8 +95,6 @@ int main(int argc, char *argv[]) {
         });
     }
 
-    QObject::connect(&app, &QApplication::aboutToQuit, []() {
-        PokemonDatabase::instance().shutdown();
-    });
+    std::unique_ptr<Game> game = std::make_unique<Game>(&engine, nullptr);
     return app.exec();
 }
